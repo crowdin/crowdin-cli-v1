0c731a1ce1da40c51e3df7d59187f4fa3381382c
Anton Maminov <anton.linux@gmail.com>
2012-10-26 10:14:49 +0300

    get_invalid_placeholders

--- lib/crowdin-cli.rb
+++ lib/crowdin-cli.rb
@@ -3,7 +3,6 @@ require 'crowdin-cli/version.rb'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
 
-require 'abbrev'
 require 'yaml'
 require 'json'
 require 'logger'
